15b4176c8cdce89f65a1fc2af722c83db055a9e2
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2021-04-30 09:38:19 +0300

    initial commit

---
+++ slugify.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "Leonard Kioi Kinyanjui"
+description   = "Convert strings to a slug. Can be used for URLs, file names, IDs etc."
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 1.4.2"
---
+++ tests/test1.nim
@@ -0,0 +1,25 @@
+# This is just an example to get you started. You may wish to put all of your
+# tests into a single file, or separate them into multiple `test1`, `test2`
+# etc. files (better names are recommended, just make sure the name starts with
+# the letter 't').
+#
+# To run these tests, simply execute `nimble test`.
+
+import unittest
+
+import slugify
+
+test "can slugify":
+  check slugify("i am a string") == "i-am-a-string"
+
+test "multiple spaces collapsed":
+  check slugify("i  am a  string") == "i-am-a-string"
+
+test "lowercase":
+  check slugify("i aM a String") == "i-am-a-string"
+
+test "no lowercase":
+  check slugify("i aM a String", false) == "i-aM-a-String"
+
+test "delimiter":
+  check slugify("i  am a  string", delimiter="=") == "i=am=a=string"
